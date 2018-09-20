185703afe004e0900442a29cd7018d0931ef2c3a
peterj330 <peter.a.jarrard@gmail.com>
2018-09-19 21:47:03 -0500

    initial directory structure and DB code

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name)
+VALUE ("Burger One");
+
+INSERT INTO burgers (burger_name)
+VALUE ("Burger Two");
+
+INSERT INTO burgers (burger_name)
+VALUE ("Burger Three");
