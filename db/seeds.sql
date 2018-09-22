6fb8afb5f995c8c1555a8c18ddbc72f8d9a9c213
peterj330 <peter.a.jarrard@gmail.com>
2018-09-21 23:38:25 -0500

    jawsDB

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,4 @@
 
-INSERT INTO burgers (burger_name) VALUES ("Burger One");
-INSERT INTO burgers (burger_name) VALUES ("Burger Two");
-INSERT INTO burgers (burger_name) VALUES ("Burger Three");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Burger Four", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Burger Five", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Burger Six", true);
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Triple Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Western Burger");
